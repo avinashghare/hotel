7e3e631923a3cee43dab3f838d8299342d3a6874
jagruti181 <patiljagruti181@gmail.com>
2015-06-09 11:10:28 +0530

    PayUMoney files added

---
+++ payumoney/paymentgateway.php
@@ -0,0 +1,61 @@
+<?php
+
+$posted = array();
+if(!empty($_GET)) {
+  foreach($_GET as $key => $value) {    
+    $posted[$key] = $value;
+  }
+}
+?>
+<html>
+  <head>
+  <script>
+    function submitPayuForm() {
+      var payuForm = document.forms.payuForm;
+      payuForm.submit();
+    }
+  </script>
+  </head>
+  <body onload="submitPayuForm()" style="display:none">
+    <h2>PayU Form</h2>
+    <form action="http://wohlig.co.in/osb/payumoney/PayUMoney_form.php" method="post" name="payuForm">
+      <table>
+        <tr>
+          <td><b>Mandatory Parameters</b></td>
+        </tr>
+        <tr>
+          <td>Amount: </td>
+          <td><input name="amount" value="<?php echo (empty($posted['amount'])) ? '' : $posted['amount'] ?>" /></td>
+          <td>First Name: </td>
+          <td><input name="firstname" id="firstname" value="<?php echo (empty($posted['firstname'])) ? '' : $posted['firstname']; ?>" /></td>
+        </tr>
+        <tr>
+          <td>Email: </td>
+          <td><input name="email" id="email" value="<?php echo (empty($posted['email'])) ? '' : $posted['email']; ?>" /></td>
+          <td>Phone: </td>
+          <td><input name="phone" value="<?php echo (empty($posted['phone'])) ? '' : $posted['phone']; ?>" /></td>
+        </tr>
+        <tr>
+          <td>Product Info: </td>
+          <td colspan="3"><textarea name="productinfo"><?php echo (empty($posted['productinfo'])) ? '' : $posted['productinfo'] ?></textarea></td>
+        </tr>
+        <tr>
+          <td>Success URI: </td>
+          <td colspan="3"><input name="surl" value="<?php echo (empty($posted['surl'])) ? '' : $posted['surl'] ?>" size="64" /></td>
+        </tr>
+        <tr>
+          <td>Failure URI: </td>
+          <td colspan="3"><input name="furl" value="<?php echo (empty($posted['furl'])) ? '' : $posted['furl'] ?>" size="64" /></td>
+        </tr>
+
+        <tr>
+          <td colspan="3"><input type="hidden" name="service_provider" value="payu_paisa" size="64" /></td>
+        </tr>
+
+        <tr>
+            <td colspan="4"><input type="submit" value="Submit" /></td>
+        </tr>
+      </table>
+    </form>
+  </body>
+</html>
